45165227a7b224b2a9e506b8caf9d79668ffff9e
ViperXYZ <sammy.haque@mail.utoronto.ca>
2017-01-21 15:48:52 -0500

    added results page

--- mirmain.py
+++ mirmain.py
@@ -39,6 +39,9 @@ def allowed_file(filename):
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/results')
+def results():
+    return render_template('result.html')
 
 
 
