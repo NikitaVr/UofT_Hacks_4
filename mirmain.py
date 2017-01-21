f7097683c1b34c596f7e0e2b5b858ed8dba64b95
ViperXYZ <sammy.haque@mail.utoronto.ca>
2017-01-21 13:05:29 -0500

    removed unnesecary code

--- mirmain.py
+++ mirmain.py
@@ -37,39 +37,6 @@ def index():
     return render_template('login.html')
 
 
-'''
-@app.route('/fileupload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return ''''''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-
-    <form action="login" method="POST">
-        <input type="text" name="username">
-        <input type="submit" name="my-form" value="Send">
-    </form>
-    '''
 
 
 
@@ -96,13 +63,6 @@ def upload():
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-'''@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
-
-
-'''
 
 @app.route('/login', methods=['POST','GET'])
 def my_form_post():
