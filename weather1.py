f908ac3f84d037efc6468d1419a543bff0e0ed63
a19c97 <a19c97@hotmail.com>
2017-01-21 00:40:55 -0500

    weather python file to get weather conditions from latitude and longitude

---
+++ weather1.py
@@ -0,0 +1,11 @@
+import urllib2
+import json
+f = urllib2.urlopen('http://api.wunderground.com/api/6a28c24f2553611a/geolookup/conditions/q/43.665301,-79.395720.json')
+json_string = f.read()
+parsed_json = json.loads(json_string)
+location = parsed_json['location']['city']
+temp_f = parsed_json['current_observation']['temp_c']
+feels_like = parsed_json['current_observation']['feelslike_c']
+print "Current temperature in %s is: %s Degrees Celsius\n" % (location, temp_f)
+print "Feels like: %s Degrees Celsius \n" % (feels_like)
+f.close()
