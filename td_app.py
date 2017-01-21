bbe5c911c9729f72ddcd86fdf632bd18d73a13cf
NikitaVr <nvarabei@gmail.com>
2017-01-21 00:12:50 -0500

    Updated app to upload files.

--- td_app.py
+++ td_app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, jsonify
+import os
+from flask import Flask, render_template, request, jsonify, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 from pprint import pprint
 from pymongo import MongoClient
 import ast
@@ -6,7 +8,13 @@ import datetime
 import time
 from time import gmtime, strftime
 
+UPLOAD_FOLDER = 'images/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 client = MongoClient('localhost',27017)
 db = client.FinhacksDB
 app_users = db.appUsers
@@ -49,9 +57,48 @@ def increment_karma(query,collection):
 def decrement_karma(query,collection):
     collection.update({"username":query["username"]},{"$inc":{"downVote":1}})
 
-@app.route("/")
-def index():
-    return "Hello World" #render_template("index.html")
+##@app.route("/")
+##def index():
+##    return "Hello World" #render_template("index.html")
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
 @app.route("/decreasekarma",methods=["POST"])
 def decrease_karma():
      json = request.json
