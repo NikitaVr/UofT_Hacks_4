b37329c55b5489ac7dd382c04d51e6c564d78e9d
Caroline Lin <Caroline Lin>
2017-01-21 10:14:47 -0500

    non-empty clarifai

---
+++ clarifai_train.py
@@ -0,0 +1,42 @@
+import os
+import json
+
+from clarifai import rest
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+
+
+os.environ["CLARIFAI_APP_ID"] = "ldgNgiUGMGdgm9M2iGKRPQxGT3s-Ds9j2wtuZODX"
+os.environ["CLARIFAI_APP_SECRET"] = "NkNVKSx41vBwAAqMe9OHAJlliFfrb3xMiIr8zzAq"
+
+
+app = ClarifaiApp()
+
+
+# get the custom model 'modelv2'
+model = app.models.get('modelv2')
+tags = ["coat"]
+
+# add the tag to concepts.
+model.add_concepts(tags)
+
+# use this file to add all images from a directory with appropriate training tags
+
+image_filepath = 'C:\\Users\\Caroline\\Desktop\\mww_coat\\'
+images = os.listdir(image_filepath)
+
+failed = 0
+total = 0
+for image in images:
+    image_path = image_filepath + image
+    # add the selected image to the application with tags
+    # batch downloader keeps corrupting download files so...
+    try:
+        json_image = app.inputs.create_image_from_filename(image_path, concepts=tags)
+    except Exception as e:
+        print("the image probably wasn't formatted correctly.")
+        failed = failed + 1
+    total = total + 1
+    json_image = app.inputs.create_image_from_filename(image_path, concepts=tags)
+
+print("failed: {}, total {}".format(failed, total))
