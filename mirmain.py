009c4c7f8b0f9b760093443dcf0c2d2b898901c2
ViperXYZ <sammy.haque@mail.utoronto.ca>
2017-01-21 06:53:12 -0500

    created file drag and drop functionality

---
+++ mirmain.py
@@ -0,0 +1,114 @@
+import os
+from flask import Flask, render_template, request, jsonify, redirect, url_for, send_from_directory,make_response
+from werkzeug.utils import secure_filename
+from pprint import pprint
+from pymongo import MongoClient
+import ast
+from datetime import datetime
+from time import gmtime, strftime
+
+
+UPLOAD_FOLDER = 'users/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+
+'''
+@app.route('/fileupload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return ''''''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+
+    <form action="login" method="POST">
+        <input type="text" name="username">
+        <input type="submit" name="my-form" value="Send">
+    </form>
+    '''
+
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            now = datetime.now()
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], "%s.%s" % (now.strftime("%Y-%m-%d-%H-%M-%S-%f"), file.filename.rsplit('.', 1)[1]))
+            file.save(filename)
+            return jsonify({"success":True})
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+'''@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+'''
+
+@app.route('/login', methods=['POST','GET'])
+def my_form_post():
+    if(request.method == 'POST'):
+        username = str(request.form['username']).strip()
+        print(username)
+        if((username!= "") and (not(os.path.isdir("users/" + username + "/")))):
+            os.makedirs("users/" + username + "/")
+            resp = make_response(render_template('index.html'))
+            resp.set_cookie('userID',username)
+            return resp
+        elif((username!= "") and (os.path.isdir("users/" + username + "/"))):
+            resp = make_response(render_template('index.html'))
+            resp.set_cookie('userID',username)
+            return resp
+
+        else:
+            return "<h1>Please enter a Username!!</h1>"
+            
+        return render_template('index.html')
+    else:
+        return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run(
+        host = "localhost",
+        port = 5000
+    )
+
